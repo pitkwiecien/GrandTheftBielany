6f27495a749cb6eabe16d8cb2ef766920b6d9bbc
figipigiel <urbmiko@gmail.com>
2026-06-03 00:26:16 +0200

    Minor main change

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "core/Game.hpp"
 
-int main(int /*argc*/, char* /*argv*/[]) {
+int main() {
     try {
         Game game;
         game.run();
     }
     catch (const std::exception& e) {
-        std::cerr << "[FATAL] " << e.what() << '\n';
+        std::cerr << e.what() << '\n';
         return 1;
     }
     return 0;
